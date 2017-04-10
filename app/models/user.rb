65e18af4b5763981e8331367158dce05610bfc92
looneym <looneymicheal@gmail.com>
2017-04-10 22:40:11 +0100

    Include refresh token in OAuth flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
+require 'json'
+
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
+    puts auth.to_json
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
+      user.refresh_token = auth.credentials.refresh_token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
