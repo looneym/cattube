960362be037745e0f5b1bb2597c0102e9b5ffe49
looneym <looneymicheal@gmail.com>
2017-04-20 21:55:38 +0100

    Modify schema and model to allow channel_ids to be persisted

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411213312) do
+ActiveRecord::Schema.define(version: 20170420204514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.text     "channel_ids", default: [],              array: true
     t.integer  "user_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "channel_ids", default: [],              array: true
     t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
   end
 
