b23aced003fc41dbeeafc30bc505c7dd5b0b68dc
looneym <looneymicheal@gmail.com>
2017-04-21 12:26:34 +0100

    Fix return value for Category helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,7 @@
 module CategoriesHelper
 
   def get_subscriptions_from_ids
-    subscriptions = Subscription.where(channel_id: @category.channel_ids)
+    Subscription.where(channel_id: @category.channel_ids)
   end
 
 end
